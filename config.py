267156b8349a90bd3f9d6ef7c72ab70ffe0272ae
Vinman <vincent.rossini@gmail.com>
2025-10-27 08:03:43 +1100

    Remove comment from Config class in config.py
    Removed unnecessary comment about database URI.

--- config.py
+++ config.py
@@ -6,6 +6,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    # The corrected database URI without the '..'
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'instance', 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
